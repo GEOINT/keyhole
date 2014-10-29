7b19ae7c3c4a87fa98d2cd9ad35a3b180585c916
steve_siebert <steve_siebert@GORD14NWKIGD097>
2014-10-29 10:53:14 -0400

    added trustmanager/sslcontext to httpclient (v3) as well

--- src/main/java/com/candlelight/keyhole/servlet/ssl/TerribleTrustManagerInstaller.java
+++ src/main/java/com/candlelight/keyhole/servlet/ssl/TerribleTrustManagerInstaller.java
@@ -6,10 +6,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import org.apache.commons.httpclient.protocol.Protocol;
+import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
+import org.jsslutils.extra.apachehttpclient.SslContextedSecureProtocolSocketFactory;
 
 /**
  *
@@ -17,18 +21,31 @@ import javax.servlet.annotation.WebListener;
 @WebListener("terribleTrustManager")
 public class TerribleTrustManagerInstaller implements ServletContextListener {
 
-    private static final Logger logger = Logger.getLogger(TerribleTrustManagerInstaller.class.getName());
+    private static final Logger logger = Logger
+            .getLogger(TerribleTrustManagerInstaller.class.getName());
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         logger.log(Level.WARNING, "This proxy currently disables ssl server"
                 + " valiation.  This will be fixed in a future version.");
+
         try {
+            //register it with the Java SSLContext
             SSLContext sc = SSLContext.getInstance("SSL");
             sc.init(null, new TrustManager[]{new TerribleTrustManager()},
                     new java.security.SecureRandom());
             HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            
+            //also register it with the Apache httpclient (sigh)
+            SslContextedSecureProtocolSocketFactory secureProtocolSocketFactory
+                    = new SslContextedSecureProtocolSocketFactory(sc);
+            secureProtocolSocketFactory.setHostnameVerification(false);
+            Protocol.registerProtocol("https", new Protocol("https",
+                    (ProtocolSocketFactory) secureProtocolSocketFactory, 443));
         } catch (GeneralSecurityException e) {
+            logger.log(Level.SEVERE, "Unable to register SSL work around, "
+                    + "requsts to HTTPS servers with invalid certificates will "
+                    + "not work.", e);
         }
     }
 
