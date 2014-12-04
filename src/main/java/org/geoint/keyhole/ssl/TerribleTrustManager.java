792ec12f59c93233ad6d15a771cd6d14514acdc3
steve_siebert <steve_siebert@GORD14NWKIGD097>
2014-12-04 09:07:34 -0500

    added some logging to the terrible trust manager

--- src/main/java/org/geoint/keyhole/ssl/TerribleTrustManager.java
+++ src/main/java/org/geoint/keyhole/ssl/TerribleTrustManager.java
@@ -1,6 +1,10 @@
 package org.geoint.keyhole.ssl;
 
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.net.ssl.TrustManager;
 
 /**
@@ -8,15 +12,28 @@ import javax.net.ssl.TrustManager;
  */
 public class TerribleTrustManager implements TrustManager {
 
+    private static final Logger logger = Logger
+            .getLogger(TerribleTrustManager.class.getName());
+
     public java.security.cert.X509Certificate[] getAcceptedIssuers() {
         return new X509Certificate[0];
     }
 
     public void checkClientTrusted(
-            java.security.cert.X509Certificate[] certs, String authType) {
+            X509Certificate[] certs, String authType) {
+        logger.log(Level.FINE, "The following client certs were approved by "
+                + "the TerribleTrustManager: {0}", certString(certs));
     }
 
     public void checkServerTrusted(
-            java.security.cert.X509Certificate[] certs, String authType) {
+            X509Certificate[] certs, String authType) {
+        logger.log(Level.FINE, "The following server certs were approved by "
+                + "the TerribleTrustManager: {0}", certString(certs));
+    }
+
+    private String certString(X509Certificate[] certs) {
+        return Arrays.stream(certs).map(
+                (c) -> c.getSubjectDN().getName())
+                .collect(Collectors.joining(","));
     }
 }
