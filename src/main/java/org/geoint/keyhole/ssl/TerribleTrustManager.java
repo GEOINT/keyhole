e7f7359d0b5b1515c6efd8325fff01a6974d1fb0
steve_siebert <steve_siebert@GORD14NWKIGD097>
2014-12-04 09:22:17 -0500

    added some logging to the terrible trust manager

--- src/main/java/org/geoint/keyhole/ssl/TerribleTrustManager.java
+++ src/main/java/org/geoint/keyhole/ssl/TerribleTrustManager.java
@@ -5,26 +5,29 @@ import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * A temporary hack until I can build the GUI to manage 3rd party certificates.
  */
-public class TerribleTrustManager implements TrustManager {
+public class TerribleTrustManager implements X509TrustManager {
 
     private static final Logger logger = Logger
             .getLogger(TerribleTrustManager.class.getName());
 
+    @Override
     public java.security.cert.X509Certificate[] getAcceptedIssuers() {
         return new X509Certificate[0];
     }
 
+    @Override
     public void checkClientTrusted(
             X509Certificate[] certs, String authType) {
         logger.log(Level.FINE, "The following client certs were approved by "
                 + "the TerribleTrustManager: {0}", certString(certs));
     }
 
+    @Override
     public void checkServerTrusted(
             X509Certificate[] certs, String authType) {
         logger.log(Level.FINE, "The following server certs were approved by "
@@ -32,6 +35,9 @@ public class TerribleTrustManager implements TrustManager {
     }
 
     private String certString(X509Certificate[] certs) {
+        if (certs == null) {
+            return "";
+        }
         return Arrays.stream(certs).map(
                 (c) -> c.getSubjectDN().getName())
                 .collect(Collectors.joining(","));
